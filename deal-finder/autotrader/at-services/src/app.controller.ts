42549909a16450b414a375d63273350bf0d059ad
Phillip Ngan <phillip.ngan@gmail.com>
2018-12-07 20:16:08 +1300

    Added stub controller methods and service methods for the menu queries to at.

--- deal-finder/autotrader/at-services/src/app.controller.ts
+++ deal-finder/autotrader/at-services/src/app.controller.ts
@@ -1,12 +1,32 @@
 import { Get, Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('at')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('ping')
   ping(): string {
     return this.appService.ping();
-    }
+  }
+
+  @Get('makes')
+  getMake(): string {
+    return this.appService.make();
+  }
+
+  @Get('models')
+  getModel(): string {
+    return this.appService.model();
+  }
+
+  @Get('searchParams')
+  getSearchParams(): string {
+    return this.appService.searchParams();
+  }
+
+  @Get('vehicles')
+  getVehicles(): string {
+    return this.appService.vehicles();
+  }
 }
--- deal-finder/autotrader/at-services/src/app.service.ts
+++ deal-finder/autotrader/at-services/src/app.service.ts
@@ -4,6 +4,18 @@ import * as moment from 'moment';
 
 @Injectable()
 export class AppService {
+  make(): string {
+    return 'make';
+  }
+  model(): string {
+    return 'model';
+  }
+  searchParams(): string {
+    return 'searchParams';
+  }
+  vehicles(): string {
+    return 'vehicles';
+  }
   ping(): string {
     return `${moment().format()}: ${hostname()}`;
   }
