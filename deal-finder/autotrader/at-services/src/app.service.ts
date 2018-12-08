0b35f538ecfbfc057e846519e98f3271d99f12a2
Phillip Ngan <phillip.ngan@gmail.com>
2018-12-08 17:36:16 +1300

    Simplified casting object to interface.

--- deal-finder/autotrader/at-services/src/app.service.ts
+++ deal-finder/autotrader/at-services/src/app.service.ts
@@ -10,7 +10,7 @@ export class AppService {
   constructor(private http: HttpService){}
 
 // Converting json to class
-// https://stackoverflow.com/questions/38688822/how-to-parse-json-string-in-typescript
+// https://stackoverflow.com/questions/43894565/cast-object-to-interface-in-typescript
   async make(): Promise<any> {
     return await this.http.get('https://www.autotrader.co.nz/search-fields/listing?searchterm=a,,,,,,',
       {
@@ -18,7 +18,8 @@ export class AppService {
           'X-Requested-With': 'XMLHttpRequest',
         },
       }).pipe(map((res) => {
-    const fieldValueSet: IFieldValues[] = JSON.parse(JSON.stringify(res.data));
+
+    const fieldValueSet: IFieldValues[] = res.data;
     return fieldValueSet.filter(x => x.FieldName === 'make');
   }));
 
