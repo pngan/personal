9ae4fe8b5bb0cc03e55db8ca206a77fed8d04e27
Phillip Ngan <phillip.ngan@gmail.com>
2019-02-20 19:53:55 +1300

    Create a query url that retrieves one page of vehcile search resuilts

--- deal-finder/autotrader/at-services/src/app.service.ts
+++ deal-finder/autotrader/at-services/src/app.service.ts
@@ -4,14 +4,15 @@ import { hostname } from 'os';
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
 import { IFieldValues, IFieldValue, QueryParams } from '../../at-shared/dto/at-dto';
+import { json } from 'body-parser';
 
 @Injectable()
 export class AppService {
   baseUrl = 'https://www.autotrader.co.nz/search-fields/listing';
-  constructor(private http: HttpService){}
+  constructor(private http: HttpService) { }
 
-// Converting json to class
-// https://stackoverflow.com/questions/43894565/cast-object-to-interface-in-typescript
+  // Converting json to class
+  // https://stackoverflow.com/questions/43894565/cast-object-to-interface-in-typescript
   async make(): Promise<any> {
     return await this.http.get(`${this.baseUrl}?searchterm=a,,,,,,`,
       {
@@ -20,9 +21,9 @@ export class AppService {
         },
       }).pipe(map((res) => {
 
-    const fieldValueSet: IFieldValues[] = res.data;
-    return fieldValueSet.filter(x => x.FieldName === 'make');
-  }));
+        const fieldValueSet: IFieldValues[] = res.data;
+        return fieldValueSet.filter(x => x.FieldName === 'make');
+      }));
   }
   async menusForMake(make: string): Promise<any> {
     Logger.log(`** Make is ${make}`);
@@ -32,8 +33,8 @@ export class AppService {
           'X-Requested-With': 'XMLHttpRequest',
         },
       }).pipe(map((res) => {
-    return res.data;
-  }));
+        return res.data;
+      }));
   }
   async menusForModel(make: string, model: string): Promise<any> {
     Logger.log(`** Make is ${make}, Model is ${model}`);
@@ -43,16 +44,25 @@ export class AppService {
           'X-Requested-With': 'XMLHttpRequest',
         },
       }).pipe(map((res) => {
-    return res.data;
-  }));
+        return res.data;
+      }));
   }
   searchParams(): string {
     return 'searchParams';
   }
   vehicles(queryParams: QueryParams): string {
-    Logger.log(`Search for ${queryParams}`);
+    Logger.log(`Search for ${JSON.stringify(queryParams)}`);
     Logger.log(`Return ${JSON.stringify('hello')}`);
-    return JSON.stringify('hello');
+
+    let url = 'https://www.autotrader.co.nz/used-cars-for-sale/';
+    if (queryParams.make !== undefined)
+      url += queryParams.make;
+    if (queryParams.model !== undefined)
+      url += '-' + queryParams.model;
+    if (queryParams.region !== undefined)
+      url += '-' + queryParams.region;
+    url += `/price-${queryParams.priceLow}-${queryParams.priceHigh}/year-${queryParams.yearLow}-${queryParams.yearHigh}/kms-${queryParams.distLow}-${queryParams.distHigh}`;
+    return JSON.stringify(url);
   }
   ping(): string {
     return `${moment().format()}: ${hostname()}`;
