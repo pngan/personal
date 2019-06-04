6c42534a4b9a130b3837829c803749cc4d60f887
Phillip Ngan <phillip.ngan@gmail.com>
2019-06-04 20:44:57 +1200

    Format search results in a responsive list using CSS grid and flex

--- deal-finder/autotrader/client/src/app/result-item/result-item.component.ts
+++ deal-finder/autotrader/client/src/app/result-item/result-item.component.ts
@@ -9,9 +9,16 @@ import { IResultDto } from '../../../../at-shared/dto/at-dto';
 export class ResultItemComponent implements OnInit {
 
   @Input() result: IResultDto;
+
+  direction: string;
+  absolutePercentage: number;
   constructor() { }
 
   ngOnInit() {
+    if (this.result !== undefined) {
+      this.direction = this.result.discount < 0 ? 'premium' : 'discount';
+      this.absolutePercentage = this.result.discount < 0 ? -this.result.discount : this.result.discount;
+    }
   }
 
 }
