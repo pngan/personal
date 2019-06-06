12ff2ae0afd99f168ffa0ae6ef8ace5312d8875b
Phillip Ngan <phillip.ngan@gmail.com>
2019-06-06 20:29:35 +1200

    Modified http listener to use the OpenShift server and port environment variables

--- deal-finder/autotrader/at-services/src/main.ts
+++ deal-finder/autotrader/at-services/src/main.ts
@@ -1,7 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+
+  const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+  const serverIp = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -10,6 +15,7 @@ async function bootstrap() {
     next();
   });
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(serverPort, serverIp);
+  Logger.log(`Listening on <${serverIp}:${serverPort}>`);
 }
 bootstrap();
