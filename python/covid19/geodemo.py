7600e7d5bcdb0b9ebd5c74b9d763d948c974f1da
Phillip Ngan <phillip.ngan@gmail.com>
2020-03-30 22:14:09 +1300

    Read csv file data into python dicts

---
+++ python/covid19/geodemo.py
@@ -0,0 +1,8 @@
+import plotly.express as px
+df = px.data.gapminder()
+print(df)
+fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
+                     hover_name="country", size="pop",
+                     animation_frame="year",
+                     projection="natural earth")
+fig.show()
--- python/covid19/geoplot.py
+++ python/covid19/geoplot.py
@@ -2,36 +2,50 @@ import requests
 from io import StringIO
 import csv
 
-data = []
-
-# Read confirmed case counts
-
-confirmed_responses = requests.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-f = StringIO(confirmed_responses.content.decode('utf-8'))
-reader = csv.reader(f, delimiter=',')
-for row in reader:
-    country_name = row[1]
-    values =  row[4:] 
-    try:
-        country_item = next( filter( lambda  x: x['country_name'] == country_name, data))
-        country_item['cases'] = values
-    except StopIteration:
-        data.append({ 'country_name': country_name, 'cases': values})
-
-# Read death counts
-
-deaths_responses = requests.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-f = StringIO(deaths_responses.content.decode('utf-8'))
-reader = csv.reader(f, delimiter=',')
-for row in reader:
-    country_name = row[1]
-    values =  row[4:] 
-    try:
-        country_item = next( filter( lambda  x: x['country_name'] == country_name, data))
-        country_item['deaths'] = values
-    except StopIteration:
-        data.append({ 'country_name': country_name, 'deaths': values})
+data = {}
 
 
-for country in data:
-    print(country)
+def read_data(data, url, statistic) :
+    confirmed_responses = requests.get(url)
+    f = StringIO(confirmed_responses.content.decode('utf-8'))
+    reader = csv.reader(f, delimiter=',')
+    header = next(reader)
+    dates = header[4:]
+    for row in reader:
+        country_name = row[1] or 'None'
+        country = data.get(country_name)
+        if (country == None):
+            data[country_name] = {}
+            country = data.get(country_name)
+
+        state_name = row[0] or 'None'
+        state = country.get(state_name)
+        if (state == None):
+            country[state_name] = {}
+            state = country.get(state_name)
+
+        datum = state.get(statistic)
+        if (datum == None):
+            state[statistic] = {}
+            datum = state.get(statistic)
+        values = row[4:]
+        value_iter = iter(values)
+        for day in dates:
+            datum[day] = next(value_iter)
+
+read_data(data,
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
+    'confirmed_cases')
+
+
+read_data(data,
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv',
+    'deaths')
+
+read_data(data,
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv',
+    'recovered')
+
+f = open("dict.json","w")
+f.write( str(data) )
+f.close()
