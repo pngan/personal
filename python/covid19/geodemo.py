39f251b9212c1f24432078122debfd738ae4d456
Phillip Ngan <phillip.ngan@gmail.com>
2020-11-27 17:23:27 +1300

    Misc changes to geoplot

--- python/covid19/geodemo.py
+++ python/covid19/geodemo.py
@@ -1,8 +1,2 @@
-import plotly.express as px
-df = px.data.gapminder()
-print(df)
-fig = px.scatter_geo(df, locations="iso_alpha", color="continent",
-                     hover_name="country", size="pop",
-                     animation_frame="year",
-                     projection="natural earth")
-fig.write_html('geodemo.html', auto_open=True)
+import geoplot
+geoplot.generate_geoplot()
